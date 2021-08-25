abd42776329aeead467ce229b7784812998b9571
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-08-25 15:08:41 +0200

    add dummy action to get in touch form

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,14 +57,13 @@ let footer = $(`
           </div>
 
 
-
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0 form-comtainer">
           <div class="form-style-6">
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
                 <form>
-                  <form action="https://formcarry.com/s/S2thQbCqEvW" method="POST" accept-charset="UTF-8" >
+                  <form action="add here the action to send you an email" method="POST" accept-charset="UTF-8" >
                   <input type="text" name="field1" placeholder="Your Name" required/>
                   <input type="email" name="field2" placeholder="Email Address"  required/>
                   <textarea name="field3" placeholder="Type your Message" required></textarea>
@@ -74,7 +73,6 @@ let footer = $(`
           </div>
         </div>
     </div>
-
 
     
 
